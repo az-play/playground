81ccfbb0b07de630840830c3133f988da98cf9e9
jm <example.com>
2025-01-13 16:59:03 +0000

    code5

--- scripts/certbot/test.sh
+++ scripts/certbot/test.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
+default_env=$(jq -r '.default_env' "$env_vars_file") 
+default_domain=$(jq -r '.default_domain' "$env_vars_file") 
+rg=$(jq -r '.RG' "$env_vars_file")
+rp=$(jq -r '.RP' "$env_vars_file")
+fqdn= "*.${default_domain}"
 echo "Running Python script..."
 
-echo "The RG IS $1"
+echo "Running Python script with default_env: $default_env, default_domain: $default_domain, RG: $rg, RP: $rp, and fqdn: $fqdn"
 python "${GITHUB_WORKSPACE}/scripts/certbot/hello.py"
 

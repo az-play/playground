8a25877680f267efaf24859813189122d7e16875
jm <example.com>
2025-01-13 18:28:22 +0000

    code12

--- scripts/certbot/test.sh
+++ scripts/certbot/test.sh
@@ -12,9 +12,10 @@ default_env=$(jq -r '.default_env' "$env_vars_file")
 default_domain=$(jq -r '.default_domain' "$env_vars_file")
 rg=$(jq -r '.RG' "$env_vars_file")
 rp=$(jq -r '.RP' "$env_vars_file")
+sub=$(jq -r '.subscription_id' "$env_vars_file")
 fqdn="*.$default_domain"
 
-echo "Running Python script with default_env: $default_env, default_domain: $default_domain, RG: $rg, RP: $rp, and fqdn: $fqdn"
+echo "Running Python script with default_env: $default_env, default_domain: $default_domain, RG: $rg, RP: $rp, and fqdn: $fqdn and sub: $sub"
 
 # Display the content of the config file
 echo "Config file content:"

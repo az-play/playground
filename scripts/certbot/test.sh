e2f61b5c122cc7ccfe3d02245d903bc50d9efee7
jm <example.com>
2025-01-13 17:08:35 +0000

    code6

--- scripts/certbot/test.sh
+++ scripts/certbot/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+config_file=$2
 default_env=$(jq -r '.default_env' "$env_vars_file") 
 default_domain=$(jq -r '.default_domain' "$env_vars_file") 
 rg=$(jq -r '.RG' "$env_vars_file")
@@ -7,5 +8,7 @@ fqdn= "*.${default_domain}"
 echo "Running Python script..."
 
 echo "Running Python script with default_env: $default_env, default_domain: $default_domain, RG: $rg, RP: $rp, and fqdn: $fqdn"
+echo "Config file content:" 
+cat "$config_file"
 python "${GITHUB_WORKSPACE}/scripts/certbot/hello.py"
 

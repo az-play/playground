3268673542944e10a5338a679d2aa27812055ffa
ijewelmas <63902199+ijewelmas@users.noreply.github.com>
2025-01-20 18:26:12 +0000

    Update hello.py

--- scripts/certbot/hello.py
+++ scripts/certbot/hello.py
@@ -7,10 +7,10 @@ import os
 # subscription_id = env_vars['subscription_id'] 
 # rp = env_vars['RP'] 
 # default_env = env_vars['default_env']
-secret = os.environ["secret"]
+secret123 = os.environ["secret123"]
 print("Hello from Python!")
 
 
-print(f"Running : {secret}")
+# print(f"Running : {secret}")
 
 

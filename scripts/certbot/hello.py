555b28279af5848c684e2601cb3caef254fb9198
ijewelmas <63902199+ijewelmas@users.noreply.github.com>
2025-01-20 17:59:43 +0000

    Update hello.py

--- scripts/certbot/hello.py
+++ scripts/certbot/hello.py
@@ -1,3 +1,16 @@
+# hello.py
+import json 
 import sys
-password = sys.argv[1]
-print(f"The generated password is: {password}")
+import os
+# json_object = sys.argv[1]
+# default_domain = env_vars['default_domain'] 
+# subscription_id = env_vars['subscription_id'] 
+# rp = env_vars['RP'] 
+# default_env = env_vars['default_env']
+
+print("Hello from Python!")
+access-key=os.environ["access-key"]
+
+print(f"Running Python script with default_env: access-key : {access-key}")
+
+
